8defc704cf60ada11ae839e0c6a00e3a3b0d69f8
Aeroblast <Aeroblast@user.norelpy.github.com>
2019-12-20 21:44:17 +0800

    触摸支持（基础）

--- AeroEpubViewer/Res/viewer-inject.js
+++ AeroEpubViewer/Res/viewer-inject.js
@@ -1,24 +1,28 @@
-﻿window.onmousewheel = function(e)
-{
-    this.parent.document.Scroll(Math.sign(e.wheelDelta)*40);
+﻿var PD = this.parent.document;
+window.onmousewheel = function (e) {
+    PD.Scroll(Math.sign(e.wheelDelta) * 40);
 }
-document.addEventListener("keydown", this.parent.document.keydown);
-document.body.onclick=this.parent.document.FrameMouseUp;
+document.addEventListener("keydown", PD.keydown);
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.body.onclick = PD.FrameMouseUp;
 var fontSizeStyle = document.createElement("style");
-fontSizeStyle.innerHTML = "body{font-size:" + this.parent.document.userSettings.bookFontSize + "px}";
+fontSizeStyle.innerHTML = "body{font-size:" + PD.userSettings.bookFontSize + "px}";
 document.head.appendChild(fontSizeStyle);
 var style = document.createElement("style");
-style.innerHTML = this.parent.document.direction.injectStyle;
+style.innerHTML = PD.direction.injectStyle;
 document.head.appendChild(style);
-function Href(e)
-{
-    if (e.getAttribute("epub:type") == "noteref")
-    {
+
+document.addEventListener("touchstart", function (e) { PD.OnFrameTouchStart(e.touches[0].screenX, e.touches[0].screenY); });
+document.addEventListener("touchend", function (e) { PD.OnFrameTouchEnd(); });
+document.addEventListener("touchmove", function (e) { PD.OnFrameTouchMove(e.touches[0].screenX, e.touches[0].screenY); });
+
+function Href(e) {
+    if (e.getAttribute("epub:type") == "noteref") {
         let noteid = e.href.split('#')[1];
         let note = document.getElementById(noteid);
-        this.parent.document.PopupFootnote(note.innerHTML, e.offsetLeft, e.offsetTop, this.frameElement);
+        PD.PopupFootnote(note.innerHTML, e.offsetLeft, e.offsetTop, this.frameElement);
         return false;
     }
-    this.parent.document.Link(e.href);
+    PD.Link(e.href);
 
 }
