d04b1fcc911582857b6d32e53fd42bf607a093f3
Aeroblast <Aeroblast@user.norelpy.github.com>
2019-12-21 14:41:15 +0800

    版本号

--- AeroEpubViewer/Properties/AssemblyInfo.cs
+++ AeroEpubViewer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("AeroEpubViewer v.0.0.0.1")]
+[assembly: AssemblyTitle("AeroEpubViewer")]
 [assembly: AssemblyDescription("https://github.com/Aeroblast/AeroEpubViewer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aeroblast")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
